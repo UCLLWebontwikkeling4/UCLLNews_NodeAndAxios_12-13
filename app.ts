f92e9d848ded7f4a368447574a6af323612afcf4
MattiasWaem <75215270+MattiasWaem@users.noreply.github.com>
2022-04-28 16:23:31 +0200

    done deelopdracht 3

--- app.ts
+++ app.ts
@@ -13,10 +13,15 @@ const server = http.createServer((req, res) => {
             res.write('Hello world!')
             res.end();
             break
-        case '/lectors':
+        case '/news/all':
             res.writeHead(200, { 'Content-Type': 'text/html' })
             // call data => build html with data => write html => end response
-            makeGetRequest().then(data => buildHtml(data)).then(html => res.write(html)).then(() => res.end())
+            makeGetRequestAll().then(data => buildHtmlForOverview(data)).then(html => res.write(html)).then(() => res.end())
+            break
+        case '/news/add':
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            // call data => build html with data => write html => end response
+            makeGetRequestAll().then(data => buildHtmlForForm()).then(html => res.write(html)).then(() => res.end())
             break
     }
 })
@@ -25,18 +30,44 @@ server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/ press Ctrl-C to terminate....`)
 })
 
-async function makeGetRequest () {
-    let res = await axios.get('http://localhost:8080/Controller?command=Overview')
+async function makeGetRequestAll () {
+    let res = await axios.get('http://localhost:8080/demo_war_exploded/Controller?command=AllNewsItems')
     return res.data
 }
 
-function buildHtml(data) {
+function buildHtmlForOverview(data) {
     let html = json2html.render(data,
-        {"<>": "li", "html":[{"<>": "span", "text": "${firstname} ${name}"}]})
+        {"<>": "li", "html":[{"<>": "span", "text": "${title} ${author} " + "${date.year} - ${date.month} - ${date.dayOfMonth}" + " ${text} ${comments}"}]})
 
     return  '<!DOCTYPE html>'+
         '<html>'+
-        '<head>LECTORS</head>'+
+        '<head>News Items</head>'+
         '<body>' + html + '</body>'+
+        '</html>'
+}
+
+function buildHtmlForForm() {
+    return  '<!DOCTYPE html>'+
+        '<html>'+
+        '<head>News Items</head>'+
+        '<body>' +
+        '<form id="addForm" method="post" action="http://localhost:8080/demo_war_exploded/Controller?command=AddNewsItem" class="bg-teal-100 rounded-lg border-2 border-teal-300 shadow-md shadow-slate-400 w-1/2 mx-auto p-2 grid grid-cols-2 gap-8">' +
+        '<div class="text-lg col-start-1 col-end-2">' +
+        '<label class="mb-2 font-bold block" for="title">Title</label>' +
+        '<input class="block p-2 rounded-md border-2 border-teal-300 w-full" type="text" name="title" id="title"/>' +
+        '</div>' +
+        '<div class="text-lg col-start-2 col-end-3">' +
+        '<label class="mb-2 font-bold block" for="author">Author</label>' +
+        '<input class="block p-2 rounded-md border-2 border-teal-300 w-full" type="text" name="author" id="author"/>' +
+        '</div>' +
+        '<div class="text-lg col-start-1 col-end-3">' +
+        '<label class="mb-2 font-bold block" for="text">Text</label>' +
+        '<input class="block p-2 rounded-md border-2 border-teal-300 w-full h-32" type="text" name="text" id="text"/>' +
+        '</div>' +
+        '<div>' +
+        '<input type="submit" id="addNewsItemButton">AddNewsItem News Item</input>' +
+        '</div>' +
+        '</form>' +
+        '</body>' +
         '</html>'
 }
